b96d4ef4f5c0d7f7423c3c7a74fe5347c224828d
Logan Elandt <elandtdevelops@gmail.com>
2020-05-13 18:11:15 -0500

    Rearrange models, and add a couple for factions and alliances

--- raid/champion_helper/models.py
+++ raid/champion_helper/models.py
@@ -2,34 +2,18 @@ from django.db import models
 
 # Create your models here.
 
+# TODO: figure out how I want the FKs to work,
+# and how to limit the relationship between
+# factions and allicances
+
 
 class Champion(models.Model):
     """
     Represents a Champion from Raid Shadow Legends
     """
 
-    name = models.CharField(max_length=100, unique=True)
-    # TODO: Figure out how I want to handle alliances...
-    # alliances have upto 4 factions, but a faction
-    # only belongs to 1 alliance
-    # May want to switch this to a foreign key
-    alliance = models.CharField(max_length=100)
-    FACTIONS = (
-        ("banner_lords", "Banner Lords"),
-        ("high_elves", "High Elves"),
-        ("sacred_order", "The Sacred Order"),
-        ("barbarians", "Barbarians"),
-        ("ogryn_tribes", "Ogryn Tribes"),
-        ("lizardmen", "Lizardmen"),
-        ("skinwalkers", "Skinwalkers"),
-        ("orcs", "Orcs"),
-        ("demonspawn", "Demonspawn"),
-        ("undead_hordes", "Undead Hordes"),
-        ("dark_elves", "Dark Elves"),
-        ("knight_revenant", "Knight Revenant"),
-        ("dwarves", "Dwarves"),
-    )
-    faction = models.CharField(max_length=50, choices=FACTIONS)
+    name = models.CharField(max_length=100, unique=True)    
+    faction = models.ForeignKey("Faction", on_delete=models.PROTECT)
     RARITIES = (
         ("c", "Common"),
         ("unc", "Uncommon"),
@@ -37,7 +21,7 @@ class Champion(models.Model):
         ("epic", "Epic"),
         ("legend", "Legendary"),
     )
-    rarity = models.CharField(max_length=25, choices=RARITIES)
+    rarity = models.CharField(max_length=7, choices=RARITIES)
     TYPES = (
         ("attack", "Attack"),
         ("defense", "Defense"),
@@ -45,8 +29,8 @@ class Champion(models.Model):
         ("support", "Support"),
     )
     type = models.CharField(max_length=7, choices=TYPES)
-    # TODO: change to either foreign key or custom model field -- does this
-    # even need to be here if there's a FK from ratings back to the champion??
+    # TODO:  does this even need to be here if
+    # there's a FK from ratings back to the champion??
     ratings = models.CharField(max_length=100)
 
     # Other possible fields: skills, num_owned, max power?
@@ -57,7 +41,8 @@ class Champion(models.Model):
 
 class Ratings(models.Model):
     """
-    Represents the ratings for a champion
+    Represents the ratings for a Champion
+    Ratings range from 0 - 5, with 1 decimal place
     """
 
     champion = models.ForeignKey(Champion, on_delete=models.CASCADE)
@@ -75,3 +60,49 @@ class Ratings(models.Model):
     force = models.DecimalField(max_digits=2, decimal_places=1)
     magic = models.DecimalField(max_digits=2, decimal_places=1)
     spirit = models.DecimalField(max_digits=2, decimal_places=1)
+
+
+class Faction(models.Model):
+    """
+    Rerpresents a faction
+    A faction has a name, and belongs to 1, and only 1, Alliance
+    """
+    FACTIONS = (
+        ("banner_lords", "Banner Lords"),
+        ("high_elves", "High Elves"),
+        ("sacred_order", "The Sacred Order"),
+        ("barbarians", "Barbarians"),
+        ("ogryn_tribes", "Ogryn Tribes"),
+        ("lizardmen", "Lizardmen"),
+        ("skinwalkers", "Skinwalkers"),
+        ("orcs", "Orcs"),
+        ("demonspawn", "Demonspawn"),
+        ("undead_hordes", "Undead Hordes"),
+        ("dark_elves", "Dark Elves"),
+        ("knight_revenant", "Knight Revenant"),
+        ("dwarves", "Dwarves"),
+    )
+    name = models.CharField(max_length=50)
+    alliance = models.ForeignKey("Alliance", on_delete=models.PROTECT)
+
+    def __str__(self):
+        return self.name
+
+
+class Alliance(models.Model):
+    """
+    Represents an Alliance
+    An Alliance has a name, and has up to 4 factions
+    """
+
+    ALLIANCES = (
+        ("telarians", "Telarians"),
+        ("gaellen_pact", "Gaellen Pact"),
+        ("corrupted", "The Corrupted"),
+        ("nyresan_union", "Nyresan Union"),
+    )
+
+    name = models.CharField(max_length=50, choices=ALLIANCES)
+
+    def __str__(self):
+        return self.name    
