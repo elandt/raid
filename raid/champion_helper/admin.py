5bf362429d9b39f4960d203650d5dbe88278c2fb
Logan Elandt <elandtdevelops@gmail.com>
2020-05-15 17:40:54 -0500

    Register the models in admin.py

--- raid/champion_helper/admin.py
+++ raid/champion_helper/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+from .models import Affinity, Alliance, Champion, Faction, Ratings
 # Register your models here.
+
+# TODO: I'll probably want to customize these admin pages later.
+admin.site.register(Affinity, Alliance, Champion, Faction, Ratings)
