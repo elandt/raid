fb681ff307ceacad01d5c5f02a7746817de424ec
Logan Elandt <elandtdevelops@gmail.com>
2020-05-15 17:15:30 -0500

    Restructure models.py so any fk models are declared before they're referenced.

--- raid/champion_helper/models.py
+++ raid/champion_helper/models.py
@@ -7,6 +7,77 @@ from django.db import models
 # factions and allicances
 
 
+class Alliance(models.Model):
+    """
+    Represents an Alliance
+    An Alliance has a name, and has up to 4 factions
+    """
+
+    ALLIANCES = (
+        ("telarians", "Telarians"),
+        ("gaellen_pact", "Gaellen Pact"),
+        ("corrupted", "The Corrupted"),
+        ("nyresan_union", "Nyresan Union"),
+    )
+
+    name = models.CharField(max_length=50, choices=ALLIANCES)
+
+    def __str__(self):
+        return self.name
+
+
+class Faction(models.Model):
+    """
+    Rerpresents a faction
+    A faction has a name, and belongs to 1, and only 1, Alliance
+    """
+
+    FACTIONS = (
+        ("banner_lords", "Banner Lords"),
+        ("high_elves", "High Elves"),
+        ("sacred_order", "The Sacred Order"),
+        ("barbarians", "Barbarians"),
+        ("ogryn_tribes", "Ogryn Tribes"),
+        ("lizardmen", "Lizardmen"),
+        ("skinwalkers", "Skinwalkers"),
+        ("orcs", "Orcs"),
+        ("demonspawn", "Demonspawn"),
+        ("undead_hordes", "Undead Hordes"),
+        ("dark_elves", "Dark Elves"),
+        ("knight_revenant", "Knight Revenant"),
+        ("dwarves", "Dwarves"),
+    )
+    name = models.CharField(max_length=50)
+    alliance = models.ForeignKey(Alliance, on_delete=models.PROTECT)
+
+    def __str__(self):
+        return self.name
+
+
+class Affinity(models.Model):
+    """
+    Represents the Affinity of a Champion
+    An Affinity has a name, and may have
+    a strength and weakness
+    """
+
+    AFFINITIES = (
+        ("force", "Force"),
+        ("magic", "Magic"),
+        ("spirit", "Spirit"),
+        ("void", "Void"),
+    )
+    name = models.CharField(max_length=6, choices=AFFINITIES)
+    strength = models.OneToOneField(
+        "Affinity",
+        on_delete=models.PROTECT,
+        null=True)
+    weakness = models.OneToOneField(
+        "Affinity",
+        on_delete=models.PROTECT,
+        null=True)
+
+
 class Champion(models.Model):
     """
     Represents a Champion from Raid Shadow Legends
@@ -21,8 +92,8 @@ class Champion(models.Model):
         ("legend", "Legendary"),
     )
     rarity = models.CharField(max_length=6, choices=RARITIES)
-    faction = models.ForeignKey("Faction", on_delete=models.PROTECT)
-    affinity = models.ForeignKey("Affinity", on_delete=models.PROTECT)
+    faction = models.ForeignKey(Faction, on_delete=models.PROTECT)
+    affinity = models.ForeignKey(Affinity, on_delete=models.PROTECT)
     TYPES = (
         ("attack", "Attack"),
         ("defense", "Defense"),
@@ -58,65 +129,3 @@ class Ratings(models.Model):
     force = models.DecimalField(max_digits=2, decimal_places=1)
     magic = models.DecimalField(max_digits=2, decimal_places=1)
     spirit = models.DecimalField(max_digits=2, decimal_places=1)
-
-
-class Faction(models.Model):
-    """
-    Rerpresents a faction
-    A faction has a name, and belongs to 1, and only 1, Alliance
-    """
-    FACTIONS = (
-        ("banner_lords", "Banner Lords"),
-        ("high_elves", "High Elves"),
-        ("sacred_order", "The Sacred Order"),
-        ("barbarians", "Barbarians"),
-        ("ogryn_tribes", "Ogryn Tribes"),
-        ("lizardmen", "Lizardmen"),
-        ("skinwalkers", "Skinwalkers"),
-        ("orcs", "Orcs"),
-        ("demonspawn", "Demonspawn"),
-        ("undead_hordes", "Undead Hordes"),
-        ("dark_elves", "Dark Elves"),
-        ("knight_revenant", "Knight Revenant"),
-        ("dwarves", "Dwarves"),
-    )
-    name = models.CharField(max_length=50)
-    alliance = models.ForeignKey("Alliance", on_delete=models.PROTECT)
-
-    def __str__(self):
-        return self.name
-
-
-class Alliance(models.Model):
-    """
-    Represents an Alliance
-    An Alliance has a name, and has up to 4 factions
-    """
-
-    ALLIANCES = (
-        ("telarians", "Telarians"),
-        ("gaellen_pact", "Gaellen Pact"),
-        ("corrupted", "The Corrupted"),
-        ("nyresan_union", "Nyresan Union"),
-    )
-
-    name = models.CharField(max_length=50, choices=ALLIANCES)
-
-    def __str__(self):
-        return self.name
-
-
-class Affinity(models.Model):
-    """
-    Represents the Affinity of a Champion
-    An Affinity has a name, 
-    """
-    AFFINITIES = (
-        ("force", "Force"),
-        ("magic", "Magic"),
-        ("spirit", "Spirit"),
-        ("void", "Void"),
-    )
-    name = models.CharField(max_length=6, choices=AFFINITIES)
-    strength = models.OneToOneField("Affinity", on_delete=models.PROTECT, null=True)
-    weakness = models.OneToOneField("Affinity", on_delete=models.PROTECT, null=True)
