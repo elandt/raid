5f7e59f6249ee428548dcf68c95c33e1e2142752
Logan Elandt <elandtdevelops@gmail.com>
2020-05-17 09:59:44 -0500

    Add champion_helper to installed apps in settings

--- raid/raid/settings.py
+++ raid/raid/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'champion_helper.apps.ChampionHelperConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
