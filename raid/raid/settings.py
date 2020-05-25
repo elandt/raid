521de52e2635cd9c8b90b92b2f30848d5122d270
Logan Elandt <elandtdevelops@gmail.com>
2020-05-25 14:44:27 -0500

    Add setting for the location of the directory containing the initial Champion data for the app. Add csv file with initial Champion data.

--- raid/raid/settings.py
+++ raid/raid/settings.py
@@ -14,7 +14,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+DATA_DIR = os.path.join(BASE_DIR, os.path.normcase("champion_helper/migrations/data/"))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
