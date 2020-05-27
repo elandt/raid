e7c752d30be981cce450163f612f8f3bd4c0494c
Logan Elandt <elandtdevelops@gmail.com>
2020-05-27 17:47:58 -0500

    Start adding frontend pages to the app. Create index.html as a Champion Summary landing page (needs work). Add bootstrap 4.5 by creating base.hmtl from the bootstrap starter template. Create a champion_helper URLConf, and map to it in the raid/urls.py URLConf. Create a base view.

---
+++ raid/champion_helper/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from . import views
+
+# sets the application namespace
+app_name = "champion_helper"
+
+urlpatterns = [
+    path("", views.IndexView.as_view(), name="index"),
+]
--- raid/champion_helper/views.py
+++ raid/champion_helper/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
+from django.views import generic
+
+from .models import Champion
+
+
 # Create your views here.
+class IndexView(generic.ListView):
+    template_name = "champion_helper/index.html"
+
+    def get_queryset(self):
+        """
+
+        """
+        order = "name"
+
+        return Champion.objects.all().order_by(order)
