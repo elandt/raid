b286edc514353e19a24614368a8f75d5484c2810
Logan Elandt <elandtdevelops@gmail.com>
2020-05-29 19:08:01 -0500

    Fix formatting.

--- raid/champion_helper/tables.py
+++ raid/champion_helper/tables.py
@@ -5,7 +5,6 @@ from .models import Champion
 class ChampionTable(tables.Table):
     ratings = tables.Column(accessor=tables.A("rating_set.all"))
 
-
     class Meta:
         model = Champion
         template_name = "django_tables2/bootstrap4.html"
