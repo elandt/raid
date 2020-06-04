a599f2c07725441e3550a51bdd059f6b235c2cf2
Logan Elandt <elandtdevelops@gmail.com>
2020-06-04 17:35:52 -0500

    Add django-filter to the project.

--- raid/raid/settings.py
+++ raid/raid/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'champion_helper.apps.ChampionHelperConfig',
     'django_tables2',
+    'django_filters',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
