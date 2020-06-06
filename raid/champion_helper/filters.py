cd0561ff8459bbe15ce02111a5a54aaf7dd3d7d7
Logan Elandt <elandtdevelops@gmail.com>
2020-06-06 00:12:36 -0500

    Fixed filtering on alliances. Still need to format the form better.

--- raid/champion_helper/filters.py
+++ raid/champion_helper/filters.py
@@ -1,26 +1,14 @@
-from django_filters import ChoiceFilter, FilterSet, ModelChoiceFilter
+from django_filters import CharFilter, ChoiceFilter, FilterSet, ModelChoiceFilter
 
 from .models import Affinity, Alliance, Champion, Faction, RARITIES, TYPES
 
 
 class ChampionFilter(FilterSet):
-    faction = ModelChoiceFilter(
-        label="Factions",
-        queryset=Faction.objects.all()
+    name = CharFilter(label="Name", lookup_expr="icontains")
+    faction__alliance = ModelChoiceFilter(
+        label="Alliances", queryset=Alliance.objects.all(),
     )
-    alliance = ModelChoiceFilter(
-        label="Alliances",
-        queryset=Alliance.objects.all()
-    )
-    rarity = ChoiceFilter(label='Type', choices=RARITIES)
-    affinity = ModelChoiceFilter(
-        label='Affinities',
-        queryset=Affinity.objects.all()
-    )
-    type = ChoiceFilter(label='Type', choices=TYPES)
 
     class Meta:
         model = Champion
-        fields = {
-            "name": ["icontains"],
-        }
+        fields = ["name", "faction", "faction__alliance", "rarity", "affinity", "type"]
