9847d7090b9185a8249ee4f03d0e9e8144acc8e7
Logan Elandt <elandtdevelops@gmail.com>
2020-06-09 22:44:58 -0500

    Changed team suggestion table queryset implementation to pull 1 queryset for all of the ratings for a given location, then slice and filter the queryset as needed for each of the tables.

--- raid/champion_helper/views.py
+++ raid/champion_helper/views.py
@@ -14,29 +14,33 @@ class IndexView(SingleTableMixin, FilterView):
 
     filterset_class = ChampionFilter
 
+# TODO: probably don't need FilterView here because it
+# would be a form for all of the tables
+
 
 class TeamSuggestionView(MultiTableMixin, FilterView):
     """
     Display team suggestions based on the selected inputs
     """
+
     model = Rating
     # TODO: Figure out what this should actually be. Need to apply
     # sanitized input from a form
-    best_overall_team = Rating.objects.filter(location__name="dragon")[:5]
-    best_force_team = Rating.objects.filter(
-        champion__affinity__name="force", location__name="dragon"
+    ratings_by_location = Rating.objects.filter(location__name="dragon")
+    best_overall_team = ratings_by_location[:5]
+    best_force_team = ratings_by_location.filter(
+        champion__affinity__name="force"
     )[:5]
-    best_magic_team = Rating.objects.filter(
-        champion__affinity__name="magic", location__name="dragon"
+    best_magic_team = ratings_by_location.filter(
+        champion__affinity__name="magic"
     )[:5]
-    best_spirit_team = Rating.objects.filter(
-        champion__affinity__name="spirit", location__name="dragon"
+    best_spirit_team = ratings_by_location.filter(
+        champion__affinity__name="spirit"
     )[:5]
-    best_void_team = Rating.objects.filter(
-        champion__affinity__name="void", location__name="dragon"
+    best_void_team = ratings_by_location.filter(
+        champion__affinity__name="void"
     )[:5]
 
-    # TODO: These should probably be rating based?
     tables = [
         # Using attrs overrides any attrs defined in the table Meta class.
         # TODO: figure out how to dynamically set the table title? Adding
@@ -48,12 +52,10 @@ class TeamSuggestionView(MultiTableMixin, FilterView):
         RatingTable(best_force_team),
         RatingTable(best_magic_team),
         RatingTable(best_spirit_team),
-        RatingTable(best_void_team)
+        RatingTable(best_void_team),
     ]
     template_name = "champion_helper/teams.html"
 
     # filterset_class = ChampionFilter
 
-    table_pagination = {
-        "per_page": 5
-    }
+    table_pagination = {"per_page": 5}
