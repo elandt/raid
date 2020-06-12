d6143524d22d0d71b6907c8b81aea30ae99e5d39
Logan Elandt <elandtdevelops@gmail.com>
2020-06-12 13:40:16 -0500

    Update filter to include Affinity because I haven't been able to get filtering to work on the multi-table version of the team suggestion page.

--- raid/champion_helper/filters.py
+++ raid/champion_helper/filters.py
@@ -1,6 +1,6 @@
 from django_filters import CharFilter, ChoiceFilter, FilterSet, ModelChoiceFilter
 
-from .models import Alliance, Champion, Faction, Location, Rating, RARITIES
+from .models import Alliance, Champion, Faction, Location, Rating, RARITIES, Affinity
 
 
 class ChampionFilter(FilterSet):
@@ -22,14 +22,14 @@ class ChampionFilter(FilterSet):
 
 
 class RatingFilter(FilterSet):
-    location__name = ModelChoiceFilter(
-        label="Location", queryset=Location.objects.all(),
-    )
     champion__faction = ModelChoiceFilter(
         label="Faction", queryset=Faction.objects.all(),
     )
     champion__faction__alliance = ModelChoiceFilter(
         label="Alliance", queryset=Alliance.objects.all(),
+    )
+    champion__affinity = ModelChoiceFilter(
+        label="Affinity", queryset=Affinity.objects.all()
     )
     champion__rarity = ChoiceFilter(
         label="Rarity", choices=RARITIES,
@@ -41,5 +41,6 @@ class RatingFilter(FilterSet):
             "champion__faction",
             "champion__faction__alliance",
             "champion__rarity",
-            "location__name"
+            "champion__affinity",
+            "location"
         ]
