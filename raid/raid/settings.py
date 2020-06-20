0b556f5ff162d50500948fbe9ebb743c9576d7af
Logan Elandt <elandtdevelops@gmail.com>
2020-06-19 21:48:24 -0500

    Added django-widget-tweaks to project, and modified form style for filters.

--- raid/raid/settings.py
+++ raid/raid/settings.py
@@ -34,6 +34,7 @@ INSTALLED_APPS = [
     'champion_helper.apps.ChampionHelperConfig',
     'django_tables2',
     'django_filters',
+    'widget_tweaks',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
